3e5a6bda6478f309ab0d91a5835b7012fb0a9ee7
RKDSpace <reshpal01@hotmail.com>
2022-09-03 18:33:02 -0700

    last push

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,11 @@
 // import the data from data.js
 const tableData = data;
 // Reference the HTML table using d3
+
+
+1
+
+
 var tbody = d3.select("tbody");
 function buildTable(data) {
     // First, clear out any existing data
@@ -25,7 +30,7 @@ function buildTable(data) {
 
   function handleClick() {
     // Grab the datetime value from the filter
-    let date = d3.select("#datetime").property("value");
+    let date = d3.select(this).property("value");
     let filteredData = tableData;
   
      // Check to see if a date was entered and filter the
@@ -34,16 +39,16 @@ function buildTable(data) {
       // Apply `filter` to the table data to only keep the
       // rows where the `datetime` value matches the filter value
       filteredData = filteredData.filter(row => row.datetime === date);
-    }
-  
+    };
+   
      // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
     buildTable(filteredData);
-  }
+  };
   
   // Attach an event to listen for the form button
-  d3.selectAll("#filter-btn").on("click", handleClick);
+  d3.selectAll("input").on("change", handleClick);
   
   // Build the table when the page loads
   buildTable(tableData);
